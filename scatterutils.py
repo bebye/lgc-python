507ad97a60ef7174b294cae132bd81a2c5548146
meryl.kwon <meryl.kwon@kakaocorp.com>
2020-06-26 20:18:47 +0900

    update logic

--- scatterutils.py
+++ scatterutils.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 
 
-def show_scatter_spirals(X, Y):
-    color = ['r' if y == 0 else 'b' for y in Y]
-    plt.scatter(X[0:, 0], X[0:, 1], c=color)
+def show_scatter_spirals(x, y):
+    plt.scatter(x[y == 0, 0], x[y == 0, 1], c='gray', marker='x', label='unlabeled')
+    plt.scatter(x[y == 1, 0], x[y == 1, 1], c='red', marker='o', label='class1')
+    plt.scatter(x[y == 2, 0], x[y == 2, 1], c='blue', marker='o', label='class2')
     plt.show()
