a9f82f9dbf05209d71c0413e4358f9b7404c0618
meryl.kwon <meryl.kwon@kakaocorp.com>
2020-06-26 20:25:32 +0900

    add label

--- scatterutils.py
+++ scatterutils.py
@@ -1,8 +1,12 @@
 import matplotlib.pyplot as plt
 
 
-def show_scatter_spirals(x, y):
-    plt.scatter(x[y == 0, 0], x[y == 0, 1], c='gray', marker='x', label='unlabeled')
+def show_scatter_spirals(x, y, is_init=False):
+    if is_init:
+        plt.scatter(x[y == 0, 0], x[y == 0, 1], c='gray', marker='x', label='unlabeled')
+
     plt.scatter(x[y == 1, 0], x[y == 1, 1], c='red', marker='o', label='class1')
     plt.scatter(x[y == 2, 0], x[y == 2, 1], c='blue', marker='o', label='class2')
+
+    plt.legend()
     plt.show()
