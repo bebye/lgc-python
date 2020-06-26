617908358c97e1723daebea61762159fc62f131a
meryl.kwon <meryl.kwon@kakaocorp.com>
2020-06-26 20:45:42 +0900

    add title and legent

--- scatterutils.py
+++ scatterutils.py
@@ -1,12 +1,17 @@
 import matplotlib.pyplot as plt
 
 
-def show_scatter_spirals(x, y, is_init=False):
+def show_scatter_spirals(x, y, title, is_init=False):
     if is_init:
         plt.scatter(x[y == 0, 0], x[y == 0, 1], c='gray', marker='x', label='unlabeled')
 
     plt.scatter(x[y == 1, 0], x[y == 1, 1], c='red', marker='o', label='class1')
     plt.scatter(x[y == 2, 0], x[y == 2, 1], c='blue', marker='o', label='class2')
 
+    plt.rc('axes', titlesize=20)
+    plt.rc('axes', labelsize=10)
+    plt.rc('legend', fontsize=10)
+
+    plt.title(title)
     plt.legend()
     plt.show()
