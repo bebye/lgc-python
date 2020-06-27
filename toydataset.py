a7a7468fe4e3e3a318ed289889a89999a895cf1c
meryl.kwon <meryl.kwon@kakaocorp.com>
2020-06-27 09:36:37 +0900

    clean up code

--- toydataset.py
+++ toydataset.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def create_spirals(n_points=1000, noise=0.5):
+def generate_spirals(n_points=1000, noise=0.5):
     """
     Generate two spirals
     :param n_points
@@ -12,5 +12,6 @@ def create_spirals(n_points=1000, noise=0.5):
     n = np.sqrt(np.random.rand(n_points, 1)) * 780 * (2 * np.pi) / 360
     dx = -np.cos(n) * n + np.random.rand(n_points, 1) * noise
     dy = np.sin(n) * n + np.random.rand(n_points, 1) * noise
+
     return (np.vstack((np.hstack((dx, dy)), np.hstack((-dx, -dy)))),
             np.hstack([np.ones(n_points), np.full(n_points, 2)]))
